7ca6218bce9e0b87f69d1cefe7d60eceb7299973
vandelio <stefanvandel@gmail.com>
2023-07-31 09:34:56 +0200

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import moment = require('moment');
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent  {
- 
-  name = "Star student";
+export class AppComponent {
+  assets = [
+    {
+      name: 'asdad',
+      actions: [
+        {
+          date: moment().add(1, 'month').add(1, 'day').calendar(),
+          action: 'Consultation Call',
+        },
+        {
+          date: moment().add(1, 'month').add(10, 'day').calendar(),
+          action: 'Preventive maintenance',
+        },
+        {
+          date: moment().add(2, 'month').add(10, 'day').calendar(),
+          action: 'Renewal engagement',
+        },
+        {
+          date: moment().add(2, 'month').add(19, 'day').calendar(),
+          action: 'Replace Air Filter',
+        },
+      ],
+    },
+  ];
 }
