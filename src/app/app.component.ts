ec4f7af8227cb5cb1879e8422954f0f7b7d5ae3a
vandelio <stefanvandel@gmail.com>
2023-08-01 10:35:24 +0200

    4 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,6 @@
 import { Component } from '@angular/core';
 import moment = require('moment');
-
-interface AssetTimelineRows {
-  actionsPerRow: number;
-  sizeOfEachAction: number;
-  timelineLength: number;
-  firstDate: string;
-  lastDate: string;
-}
-
-interface AssetInTimeline {
-  name: string;
-  actions: AssetActions[];
-}
-interface AssetActions {
-  date: moment.Moment;
-  action: string;
-  type?: string; // used to identify the first inject : today block
-}
+import { AssetInTimeline } from './timeline/timeline.component';
 
 @Component({
   selector: 'my-app',
@@ -127,77 +110,5 @@ export class AppComponent {
     },
   ];
 
-  timelinePeriod: number = 0;
-  TSfirstDate: number;
-  TSlastDate: number;
-  firstDate: moment.Moment;
-  lastDate: moment.Moment;
-  assetRows: AssetTimelineRows[] = [];
-  sizeOfEachActionLabel: number;
-  eachActionSizePercent: number;
-
-  ngOnInit() {
-    this.loopAssets();
-  }
-
-  loopAssets() {
-    // Get first date and last date
-    // Set timeline row, count per row and size per action
-    this.assets.forEach((asset, index) => {
-      asset.actions.unshift({
-        date: moment(),
-        action: 'Today',
-        type: 'first',
-      });
-      asset.actions.find((action) => {
-        if (
-          typeof this.TSfirstDate === 'undefined' ||
-          action.date.isBefore(this.TSfirstDate)
-        ) {
-          this.firstDate = action.date;
-          this.TSfirstDate = Number(action.date.format('X'));
-        }
-
-        if (
-          typeof this.TSlastDate === 'undefined' ||
-          action.date.isAfter(this.TSlastDate)
-        ) {
-          this.lastDate = action.date;
-          this.TSlastDate = Number(action.date.format('X'));
-        }
-
-        this.timelinePeriod = this.TSlastDate - this.TSfirstDate;
-        // get number of actions per row, per asset
-        this.setTimelineRow(
-          index,
-          Math.round(asset.actions.length / 2),
-          this.timelinePeriod,
-          this.firstDate,
-          this.lastDate
-        );
-      });
-    });
-
-    console.log('this.TSlastDate', this.TSlastDate);
-    console.log('this.TSfirstDate', this.TSfirstDate);
-  }
-
-  convertSizeToPercent(timelinePeriod, size) {
-    return (size / 100 / (timelinePeriod / 100)) * 100;
-  }
-
-  setTimelineRow(index, perRow, timelinePeriod, firstDate, lastDate) {
-    // actions in row / full timeline length = size of each action label
-    this.assetRows[index] = {
-      sizeOfEachAction: this.convertSizeToPercent(
-        timelinePeriod,
-        Number(timelinePeriod / perRow)
-      ),
-      actionsPerRow: perRow,
-      timelineLength: timelinePeriod,
-      firstDate: firstDate,
-      lastDate: lastDate,
-    } as AssetTimelineRows;
-    console.log(this.assetRows);
-  }
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, TimelineComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
